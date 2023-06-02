5a7f9e06a26f3c6f6685a4864acb553d491bb86f
jyothimb2396 <jyosamp290421@gmail.com>
2023-06-02 12:14:18 +0530

    JNK COMMIT

---
+++ com.onlineBanking.steller/src/test/java/Practiceprgm/Demo1.java
@@ -0,0 +1,19 @@
+package Practiceprgm;
+
+import org.testng.annotations.Test;
+
+public class Demo1 
+{
+	@Test
+	public void hiii()
+	{
+		System.out.println("-----hiiii----");
+	}
+	@Test
+	public void hello()
+	{
+		System.out.println("-----hello----");
+		System.out.println("------testcase is completed-------");
+	}
+
+}
